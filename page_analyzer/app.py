aabf25c5a9eb4503f51901932fd8e310be5efc22
ValeriaLukovich <valeriialukovich@Valeriias-MacBook-Air.local>
2024-01-24 14:09:16 +0100

    second

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def first_page():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
